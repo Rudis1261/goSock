a472ccbb4797a982dc3a6ceb8e882422c9b871bb
drpain <iam@thatguy.co.za>
2015-08-13 16:20:38 -0400

    Socket to me

---
+++ goSockServer.go
@@ -0,0 +1,51 @@
+package main
+import (
+	"log"
+	"net"
+//	"os"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgchan := make(chan string)
+	go printMessages(msgchan)
+	
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConnection(conn, msgchan)
+	}
+}
+
+func handleConnection(c net.Conn, msgchan chan<- string) {
+	buf := make([]byte, 4096)
+
+	for {
+		n, err := c.Read(buf)
+		if err != nil || n == 0 {
+			c.Close()
+			break
+		}
+	
+		msgchan <- string(buf[0:n])
+		n, err = c.Write(buf[0:n])
+		if err != nil {
+			c.Close()
+			break
+		}
+	}
+	log.Printf("Connection from %v closed.", c.RemoteAddr())
+}
+
+func printMessages(msgchan <-chan string) {
+	for msg := range msgchan {
+		log.Printf("new message: %s", msg)
+	}
+}
